80e6b22f979349c123e1e4eb03cf5869207bde33
infinitemax <m.brookman.byrne@gmail.com>
2023-09-13 09:55:26 +0100

    add function to retry btn, insert review btn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let questionState = 1;
 
 let numberOfQuestions = document.querySelectorAll(".myQuestion").length;
@@ -6,69 +5,78 @@ let numberOfQuestions = document.querySelectorAll(".myQuestion").length;
 // update heading to show number of questions
 document.getElementById("numOfQuestionsHeading").innerHTML = numberOfQuestions;
 
-console.log(`there are ${numberOfQuestions} questions in this quiz`)
-
+console.log(`there are ${numberOfQuestions} questions in this quiz`);
 
 // navigation function - hides current question div, shows next/prev one.
 const otherClicked = (direction) => {
-    
-    document.getElementById("que" + questionState).classList.add("hidden");
-    document.getElementById("que" + (questionState + direction)).classList.remove("hidden");
-    questionState = questionState + direction;
-    if (questionState > 1) {
-        document.getElementById("prevButton").classList.remove("hidden");
-    } else {
-        document.getElementById("prevButton").classList.add("hidden");
-    }
-    if (questionState === numberOfQuestions) {
-        document.getElementById("nextButton").classList.add("hidden");
-    } else {
-        document.getElementById("nextButton").classList.remove("hidden");
-    }
-}
-
-
+  document.getElementById("que" + questionState).classList.add("hidden");
+  document
+    .getElementById("que" + (questionState + direction))
+    .classList.remove("hidden");
+  questionState = questionState + direction;
+  if (questionState > 1) {
+    document.getElementById("prevButton").classList.remove("hidden");
+  } else {
+    document.getElementById("prevButton").classList.add("hidden");
+  }
+  if (questionState === numberOfQuestions) {
+    document.getElementById("nextButton").classList.add("hidden");
+  } else {
+    document.getElementById("nextButton").classList.remove("hidden");
+  }
+};
 
 //KEEPING TRACK OF THE ANSWERS
 // initialise result array
 const runningResult = [];
 
-for (let i = 0; i < numberOfQuestions; i++){
-    runningResult.push(null);
+for (let i = 0; i < numberOfQuestions; i++) {
+  runningResult.push(null);
 }
 
 const answer = (value) => {
-    runningResult[questionState - 1] = value;
-    console.log(runningResult)
-}
+  runningResult[questionState - 1] = value;
+  console.log(runningResult);
+};
+
+const buttons = document.querySelectorAll(".navButton");
 
 
 // SUBMITTING THE QUIZ
-const submitQuiz = () => {
-    document.getElementById("que" + questionState).classList.add("hidden");
-    document.getElementById("resultsDiv").classList.remove("hidden");
-    
-    const result = calculateFinalScore(runningResult);
-    console.log(result)
 
-    document.getElementById("totalSpan").innerHTML = result;
-}
+let finished = false;
+
+const submitQuiz = () => {
+  document.getElementById("que" + questionState).classList.add("hidden");
+  
+  document.getElementById("resultsDiv").classList.remove("hidden");
+  buttons.forEach(button => {
+    button.classList.add("hidden");
+  })
+
+  const result = calculateFinalScore(runningResult);
+  console.log(result);
+
+  document.getElementById("totalSpan").innerHTML = result;
+  finished = true;
+};
 
 // CALCULATE SCORE
-
 const calculateFinalScore = (resultArray) => {
-    let total = 0;
-    resultArray.forEach(item => {
-        if (item === "true") {
-            total++;
-        }
-    })
-    return total;
-}
+  let total = 0;
+  resultArray.forEach((item) => {
+    if (item === "true") {
+      total++;
+    }
+  });
+  return total;
+};
+
+
+//
 
 
 //#region previous attempts
-
 
 // let forms = document.querySelectorAll("form");
 
@@ -80,10 +88,8 @@ const calculateFinalScore = (resultArray) => {
 //     });
 // })
 
-
-
 // const giveAnswer = (result) => {
-    
+
 //     runningResult.push(result);
 //     console.log(runningResult);
 // }
